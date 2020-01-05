8ba2cacb2243b2810773816a57a304f7674fce82
Nunya <nunyaklah@gmail.com>
2020-01-05 10:50:27 +0000

    Add bitcoin average api to get price

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require(request);
+const request = require("request");
 
 const app = express();
 
@@ -11,9 +11,27 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response , body){
+   
+   var crypto = req.body.crypto;
+   var fiat = req.body.fiat;
 
-    })
+   var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+
+   var finalURL = baseURL + crypto + fiat;
+   
+    request(finalURL, function(error, response , body){
+        
+        var data =JSON.parse(body);
+        var price = data.last;
+
+        var currentDate = data.display_timestamp;
+
+        res.write("<p>The current date is " + currentDate+  " </p>");
+
+        res.write("<h1> The current price of " + crypto + "is " + price + fiat + "</h1>");
+
+        res.send();
+    });
 });
 
 app.listen(3000, function(){
